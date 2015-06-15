253ac7e190c3365ee5a5c95d98d50b318ac84b0e
Kevin Lynch <klynch@squarespace.com>
2015-06-15 11:38:23 -0400

    ignore the port when filtering carbon instances

--- carbonate/sieve.py
+++ carbonate/sieve.py
@@ -2,6 +2,7 @@ from functools import partial
 
 map_long = partial(map, lambda m: ':'.join(map(str, m)))
 map_short = partial(map, lambda m: m[0])
+map_label = partial(map, lambda m: ':'.join(map(str, m[::2])))
 
 
 def filterMetrics(inputs, node, cluster, invert=False, filter_long=False):
@@ -12,7 +13,7 @@ def filterMetrics(inputs, node, cluster, invert=False, filter_long=False):
 
     for metric_name in inputs:
         dests = list(cluster.getDestinations(metric_name))
-        dests = set(map_long(dests)) | set(map_short(dests))
+        dests = set(map_long(dests)) | set(map_short(dests)) | set(map_label(dests))
 
         if dests & set(match):
             if not invert:
