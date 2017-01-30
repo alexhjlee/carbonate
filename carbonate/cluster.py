dc0ca59e68102e9a2b0f8ad405db822fd1de4c5d
Corentin Chary <c.chary@criteo.com>
2017-01-30 10:13:48 +0100

    carbonate: support new versions of carbon
    Signed-off-by: Corentin Chary <c.chary@criteo.com>
    Signed-off-by: William Dauchy <w.dauchy@criteo.com>

--- carbonate/cluster.py
+++ carbonate/cluster.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import inspect
 
 # Inject the graphite libs into the system path
 venv_root = ""
@@ -11,21 +12,30 @@ sys.path.insert(0, venv_root + "/opt/graphite/lib")
 # We're going to use carbon's libs directly to do things
 try:
     from carbon import util
-    from carbon.aggregator.rules import RuleManager
-    from carbon.routers import AggregatedConsistentHashingRouter, ConsistentHashingRouter
+    from carbon.routers import ConsistentHashingRouter
+    from carbon.hashing import ConsistentHashRing
 except ImportError as e:
     raise SystemExit("No bueno. Can't import carbon! (" + str(e) + ")")
 
 
 class Cluster():
-    def __init__(self, config, cluster='main', aggregation_rules=None):
-        relay_method = config.relay_method(cluster=cluster)
-        if relay_method == "consistent-hashing":
-            self.ring = ConsistentHashingRouter(config.replication_factor(cluster))
-        elif relay_method == "aggregated-consistent-hashing":
-            if aggregation_rules:
-                RuleManager.read_from(aggregation_rules)
-            self.ring = AggregatedConsistentHashingRouter(RuleManager, config.replication_factor(cluster))
+    def __init__(self, config, cluster='main'):
+        # Support multiple versions of carbon, the API changed in 0.10.
+        args = inspect.getargspec(ConsistentHashingRouter.__init__).args
+        if 'replication_factor' in args:
+            r = ConsistentHashingRouter(config.replication_factor(cluster))
+        else:
+            class Settings(object):
+                REPLICATION_FACTOR = config.replication_factor(cluster)
+                DIVERSE_REPLICAS = False
+            r = ConsistentHashingRouter(Settings())
+
+        # 'hash_type' was added only in carbon 1.0.2 or master
+        args = inspect.getargspec(ConsistentHashRing.__init__).args
+        if 'hash_type' in args:
+            r.ring = ConsistentHashRing(hash_type=config.hashing_type(cluster))
+
+        self.ring = r
 
         try:
             dest_list = config.destinations(cluster)
